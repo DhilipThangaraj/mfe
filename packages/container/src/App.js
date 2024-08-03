0b105b94ef0264a41988095d3a502837fdcec2bc
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2024-08-03 12:47:18 +0400

    finished feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,9 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there from container from Micro Front End</h1>
+      <h1>
+        Hi there from container from Micro Front End!!! Its a biggest change.
+      </h1>
       <hr />
       <MarketingApp />
     </div>
