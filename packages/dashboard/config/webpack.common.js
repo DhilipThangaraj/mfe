78eeefb930389ebee2f61c933c9f7acf7e61858e
dhilipkumar thangaraj <dtdhilip@dt.ae>
2024-08-05 16:33:02 +0400

    final update

--- packages/dashboard/config/webpack.common.js
+++ packages/dashboard/config/webpack.common.js
@@ -1,35 +1,35 @@
-const { VueLoaderPlugin } = require("vue-loader");
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "[name].[contenthash].js",
+    filename: '[name].[contenthash].js',
   },
   resolve: {
-    extensions: [".js", ".vue"],
+    extensions: ['.js', '.vue'],
   },
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
-        use: [{ loader: "file-loader" }],
+        use: [{ loader: 'file-loader' }],
       },
       {
         test: /\.vue$/,
-        use: "vue-loader",
+        use: 'vue-loader',
       },
       {
         test: /\.scss|\.css$/,
-        use: ["vue-style-loader", "style-loader", "css-loader", "sass-loader"],
+        use: ['vue-style-loader', 'style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.m?js$/, //it looks for the mjs and js file
-        exclude: /node_modules/, //exclude the node
+        test: /\.m?js$/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader", //we are using the babel loader
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"], //this is for 1.handles jsx and 2.convert es5,6,7 ... to es5
-            plugins: ["@babel/plugin-transform-runtime"], //this handles async and await syntax
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,35 +1,34 @@
-const { merge } = require("webpack-merge"); //this merge two webpack configs
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: "development",
+  mode: 'development',
   output: {
-    publicPath: "http://localhost:8083/",
+    publicPath: 'http://localhost:8083/',
   },
   devServer: {
     port: 8083,
-    // historyApiFallback: {
-    //   index: "/index.html",
-    // },
-    historyApiFallback: true,
+    historyApiFallback: {
+      historyApiFallback: true,
+    },
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Origin': '*',
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new ModuleFederationPlugin({
-      name: "dashboard",
-      filename: "remoteEntry.js",
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./DashboardApp": "./src/bootstrap.js",
+        './DashboardApp': './src/bootstrap',
       },
-      shared: packageJson.dependencies, //webpack creates automatically for us array of string
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
